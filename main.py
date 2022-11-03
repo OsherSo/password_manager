5699f0c437e9fe4c9cbb4ef73c92d25e4796d525
Osher Solimany <solimani.osher@gmail.com>
2022-11-03 17:06:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
-from tkinter import messagebox
+import json
 
+from tkinter import messagebox
 from random import choice, randint, shuffle
 
 LETTERS = [
@@ -27,14 +28,16 @@ class App(tk.Frame):
         self.password_label = tk.Label(text="Password:")
         self.password_label.grid(row=3, column=0)
         # Entry's
-        self.website_entry = tk.Entry(width=35)
-        self.website_entry.grid(row=1, column=1, columnspan=2)
+        self.website_entry = tk.Entry(width=20)
+        self.website_entry.grid(row=1, column=1)
         self.website_entry.focus()
         self.email_entry = tk.Entry(width=35)
         self.email_entry.grid(row=2, column=1, columnspan=2)
         self.password_entry = tk.Entry(width=35)
         self.password_entry.grid(row=3, column=1, columnspan=2)
         # Buttons
+        self.search_button = tk.Button(text="Search", command=self.find_password)
+        self.search_button.grid(row=1, column=2)
         self.generate_password_button = tk.Button(text="Generate Password", command=self.generate_password)
         self.generate_password_button.grid(row=4, column=1, columnspan=2)
         self.add_button = tk.Button(text="Add", width=28, command=self.save)
@@ -56,16 +59,44 @@ class App(tk.Frame):
         website = self.website_entry.get()
         email = self.email_entry.get()
         password = self.password_entry.get()
+        new_data = {
+            website: {
+                "email": email,
+                "password": password,
+            }
+        }
 
         if len(website) == 0 or len(email) == 0 or len(password) == 0:
             messagebox.showinfo(title="Error", message="Please make sure you haven't left any fields empty.")
-        elif messagebox.askokcancel(title=website,
-                                    message=f"These are the details entered: \nEmail: {email}\nPassword: "
-                                            f"{password}\nIs it ok to save?"):
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
+        else:
+            try:
+                with open("data.json", "r") as data_file:
+                    data = json.load(data_file)
+            except FileNotFoundError:
+                with open("data.json", "w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+            else:
+                data.update(new_data)
+                with open("data.json", "w") as data_file:
+                    json.dump(data, data_file, indent=4)
+            finally:
                 self.website_entry.delete(0, "end")
                 self.password_entry.delete(0, "end")
+
+    def find_password(self):
+        website = self.website_entry.get()
+        try:
+            with open("data.json") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            messagebox.showinfo(title="Error", message="No Data File Found.")
+        else:
+            if website in data:
+                email = data[website]["email"]
+                password = data[website]["password"]
+                messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+            else:
+                messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
 
 
 def main():
